f5ff3551c3de2b23c5acd46f65a1aeb363118475
harmony <harmony.dgt@gmail.com>
2023-10-21 21:29:09 -0300

    up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
+import logo from './img/logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logox" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Uma dieta mais saudável.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <butoom
+          className="btn-circle"
+         
         >
-          Learn React
-        </a>
+          .
+        </butoom>
       </header>
     </div>
   );
