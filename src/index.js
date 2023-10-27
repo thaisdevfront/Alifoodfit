645b321a8ce2e0790b83b2a470816cd98f8633c2
harmony <harmony.dgt@gmail.com>
2023-10-27 06:06:15 -0300

    up-comp

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { reportWebVitals } from 'react';
+import { reportWebVitals } from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root')
 );
@@ -16,4 +16,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
-reportWebVitals()
+reportWebVitals();
